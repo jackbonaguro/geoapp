b92c55118b3342e3e8da43913edf1531fd19a1c4
Zachary Cheshire <zcheshire3@gatech.edu>
2017-02-15 00:04:48 -0500

    Added Graffit IOS project

---
+++ Graffit/Graffit/model.swift
@@ -0,0 +1,13 @@
+//
+//  model.swift
+//  Graffit
+//
+//  Created by Zachary Cheshire on 2/14/17.
+//  Copyright © 2017 zcheshire. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let user:NSMutableDictionary = NSMutableDictionary()
+
